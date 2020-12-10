3514326e8501b62d26833520735c4a25687f5274
greb <greb>
2020-12-10 12:52:08 +0100

    Solve day1

--- days/day1.py
+++ days/day1.py
@@ -1,5 +1,19 @@
 def solve_part1(inp):
-    return 'part1'
+    entries = sorted(int(e) for e in inp.split())
+
+    for i, a in enumerate(entries):
+        for b in entries[i+1:]:
+            if a+b == 2020:
+                return a*b
 
 def solve_part2(inp):
-    return 'part2'
+    entries = sorted(int(e) for e in inp.split())
+
+    for i, a in enumerate(entries):
+        for j, b in enumerate(entries[i+1:]):
+            for c in entries[j+1:]:
+                s = a+b+c
+                if s > 2020:
+                    break
+                if s == 2020:
+                    return a*b*c
