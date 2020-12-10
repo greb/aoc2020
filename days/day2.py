2e6a28e28e8d27f63e978c28336a6e92644c66a0
greb <greb>
2020-12-10 13:24:16 +0100

    Solve day2

---
+++ days/day2.py
@@ -0,0 +1,34 @@
+import re
+
+def parse(inp):
+    pattern = re.compile(r'(\d*)-(\d*) (.): (.*)')
+    return pattern.findall(inp)
+
+def solve_part1(inp):
+    inp = parse(inp)
+
+    correct_cnt = 0
+    for lo, hi, ch, pw in inp:
+        cnt = pw.count(ch)
+        lo = int(lo)
+        hi = int(hi)
+
+        if lo <= cnt <= hi:
+            correct_cnt += 1
+
+    return correct_cnt
+
+
+def solve_part2(inp):
+    inp = parse(inp)
+
+    correct_cnt = 0
+    for lo, hi, ch, pw in inp:
+        lo = pw[int(lo) - 1] == ch
+        hi = pw[int(hi) - 1] == ch
+
+        if (lo and not hi) or (not lo and hi):
+            correct_cnt += 1
+
+    return correct_cnt
+
