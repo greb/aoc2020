93848b54f28794e422835b4b88f3591e6eeb6069
greb <greb>
2020-12-10 16:20:44 +0100

    Solve day6

---
+++ days/day6.py
@@ -0,0 +1,32 @@
+def parse_groups(inp):
+    group = list()
+    for line in inp.splitlines():
+        if not line:
+            yield group
+            group = list()
+        else:
+            group.append(line)
+    yield group
+
+
+def solve_part1(inp):
+    cnt = 0
+
+    for group in parse_groups(inp):
+        ans = set()
+        for g in group:
+            ans.update(g)
+        cnt += len(ans)
+    return cnt
+
+def solve_part2(inp):
+    cnt = 0
+
+    for group in parse_groups(inp):
+        ans = set(group[0])
+        for g in group:
+            ans.intersection_update(g)
+        cnt += len(ans)
+    return cnt
+
+
