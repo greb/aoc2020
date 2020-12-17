e02fc255aa03de61324745b548a35a4fed31d5b3
greb <greb>
2020-12-17 10:39:57 +0100

    Refactor day17
    * Apply some itertools magic to find neighbors
    * Instead of using bounds and do O(n^3) or O(n^4) search, keep counts
      in a seperate dict. It's much faster and easier on the eyes.

--- days/day17.py
+++ days/day17.py
@@ -1,133 +1,51 @@
-def parse(inp):
+import itertools
+import collections
+
+def parse(inp, dims):
     cubes = set()
-    lines = inp.splitlines()
 
-    z = 0
-    for y, line in enumerate(lines):
-        for x, cell in enumerate(line):
-            if cell == '#':
-                cubes.add((x,y,z))
-
+    for y, line in enumerate(inp.splitlines()):
+        for x, cube in enumerate(line):
+            if cube == '#':
+                coord = tuple([x,y] + [0]*(dims-2))
+                cubes.add(coord)
     return cubes
 
 
-def cube_bounds(cubes):
-    bounds = []
-    for axis in range(3):
-        vals = [a[axis] for a in cubes]
-        bounds.append((min(vals), max(vals)))
-    return bounds
+def neigbors(cube):
+    dims = len(cube)
+    deltas = itertools.product([-1, 0, 1], repeat=dims)
+    for delta in deltas:
+        coord = tuple(c+d for c,d in zip(cube, delta))
+        if coord != cube:
+            yield coord
 
 
-def cube_neighbors(cube):
-    cx,cy,cz = cube
-    for x in range(cx-1, cx+2):
-        for y in range(cy-1, cy+2):
-            for z in range(cz-1, cz+2):
-                neighbor = (x,y,z)
-                if neighbor != cube:
-                    yield neighbor
-
-
-def cube_active(cube, cubes):
-    cnt = 0
-    for n in cube_neighbors(cube):
-        if n in cubes:
-            cnt += 1
-
-    if cube in cubes:
-        return cnt in [2,3]
-
-    return cnt == 3
-
-
-def cube_cycle(cubes):
-    (xmin,xmax), (ymin,ymax), (zmin,zmax) = cube_bounds(cubes)
+def cycle(cubes):
+    neighbor_counts = collections.defaultdict(int)
+    for cube in cubes:
+        for neighbor in neigbors(cube):
+            neighbor_counts[neighbor] += 1
 
     new_cubes = set()
-    for x in range(xmin-1, xmax+2):
-        for y in range(ymin-1, ymax+2):
-            for z in range(zmin-1, zmax+2):
-                cube = (x,y,z)
-                if cube_active(cube, cubes):
-                    new_cubes.add(cube)
+    for cube, count in neighbor_counts.items():
+        if cube in cubes:
+            if count in (2,3):
+                new_cubes.add(cube)
+        else:
+            if count == 3:
+                new_cubes.add(cube)
 
     return new_cubes
-
-
-def parse4(inp):
-    cubes = set()
-    lines = inp.splitlines()
-
-    z = 0
-    w = 0
-    for y, line in enumerate(lines):
-        for x, cell in enumerate(line):
-            if cell == '#':
-                cubes.add((x,y,z,w))
-
-    return cubes
-
-
-def cube_bounds4(cubes):
-    bounds = []
-    for axis in range(4):
-        vals = [a[axis] for a in cubes]
-        bounds.append((min(vals), max(vals)))
-    return bounds
-
-
-def cube_neighbors4(cube):
-    cx,cy,cz,cw = cube
-    for x in range(cx-1, cx+2):
-        for y in range(cy-1, cy+2):
-            for z in range(cz-1, cz+2):
-                for w in range(cw-1, cw+2):
-                    neighbor = (x,y,z,w)
-                    if neighbor != cube:
-                        yield neighbor
-
-
-def cube_active4(cube, cubes):
-    cnt = 0
-    for n in cube_neighbors4(cube):
-        if n in cubes:
-            cnt += 1
-
-    if cube in cubes:
-        return cnt in [2,3]
-
-    return cnt == 3
-
-
-def cube_cycle4(cubes):
-    (xmin,xmax), (ymin,ymax), (zmin,zmax), (wmin, wmax) = cube_bounds4(cubes)
-
-    new_cubes = set()
-    for x in range(xmin-1, xmax+2):
-        for y in range(ymin-1, ymax+2):
-            for z in range(zmin-1, zmax+2):
-                for w in range(wmin-1, wmax+2):
-                    cube = (x,y,z,w)
-                    if cube_active4(cube, cubes):
-                        new_cubes.add(cube)
-
-    return new_cubes
-
 
 def solve_part1(inp):
-    cubes = parse(inp)
-
-    for cycle in range(6):
-        cubes = cube_cycle(cubes)
-
+    cubes = parse(inp, 3)
+    for _ in range(6):
+        cubes = cycle(cubes)
     return len(cubes)
 
 def solve_part2(inp):
-    cubes = parse4(inp)
-
-    for cycle in range(6):
-        cubes = cube_cycle4(cubes)
-
+    cubes = parse(inp, 4)
+    for _ in range(6):
+        cubes = cycle(cubes)
     return len(cubes)
-
